9bf6fe489e462694799ad34be5143864d7a9eb9f
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-02 20:28:57 +0200

    add history

--- src/components/ButtonHistory/index.js
+++ src/components/ButtonHistory/index.js
@@ -4,7 +4,7 @@ import { Button } from './styles.js';
 export default function ButtonHistory() {
     return (
         <Button>
-            <a href="../../pages/history/index.js">
+            <a href="/history">
                 View conversion history >
         </a>
         </Button>

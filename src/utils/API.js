43882ae74ca82cc4482119ec845d59c6ad18efa2
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-04 17:36:43 +0200

    update

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "http://apilayer.net/api/live";
-const APIKEY = " ?access_key=c9aea1c61115f79a367ae98929ce1195";
-
-export default {
-    search: function (query) {
-        return axios.get(BASEURL + query + APIKEY);
-    }
-};

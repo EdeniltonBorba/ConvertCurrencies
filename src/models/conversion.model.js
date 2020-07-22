a52e35e07024027359afe0b509d55bab6eb1ed9e
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-22 18:43:23 +0200

    add models

--- src/models/conversion.model.js
+++ src/models/conversion.model.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
 
-const 
+const DataSchema = new mongoose.Schema({
+    conversion_value: Number,
+    conversion_currency: String,
+    converted_value: Number,
+    converted_currency: String
+}, {
+    timestamps: true
+})
+
+const conversion = mongoose.model('conversionHistory', DataSchema);
+module.exports = conversion;

8a79371a7bca38d9b5f36202b7daeca7002a3772
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-22 17:08:34 +0200

    update

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const path = require('path');
-const app = express();
+
 const PORT = process.env.PORT || 9000;
 
-app.use(express.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost/conversionhistory', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('MongoDB successfully connected.')
+    }
+});
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-    app.use('/static', express.static(path.join(__dirname, 'public')))
-}
-
-
 app.use(routes);
-
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/conversionhistory", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(PORT, function () {
     console.log(`API Server now listening on PORT ${PORT}!`)

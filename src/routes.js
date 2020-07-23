384d24634338f520987caeec113ec8b59d1fd3a8
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-23 15:01:09 +0200

    add routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,15 @@
 const express = require('express');
 
-const Conversion = require('');
+const Conversion = require('./controllers/Conversion.Controller');
+
+const routes = express.Router();
+
+routes.get('/', Conversion.index);
+
+routes.post('/api/conversion', Conversion.create);
+routes.get('/api/conversion', Conversion.index);
+routes.get('/api/conversion/details', Conversion.details);
+routes.delete('/api/conversion/:_id', Conversion.delete);
+routes.put('/api/conversion', Conversion.update);
+
+module.exports = routes;

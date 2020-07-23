a754ef984de9788483a3955a2f16a57184e55305
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-23 17:38:43 +0200

    add model

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ routes.get('/', Conversion.index);
 
 routes.post('/api/conversion', Conversion.create);
 routes.get('/api/conversion', Conversion.index);
-routes.get('/api/conversion/details', Conversion.details);
+routes.get('/api/conversion/details/:_id', Conversion.details);
 routes.delete('/api/conversion/:_id', Conversion.delete);
 routes.put('/api/conversion', Conversion.update);
 
